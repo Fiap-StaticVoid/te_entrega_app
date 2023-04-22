8f9ad33329ce6a6024e07992d86da401fae499a9
Gabriela Matias <jasbrela@gmail.com>
2023-04-21 21:35:45 -0300

    register first page + change all tools:text to android:text

--- app/src/main/java/com/example/teentrega/MainActivity.kt
+++ app/src/main/java/com/example/teentrega/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.teentrega
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEnter = findViewById<Button>(R.id.button_register)
+
+        buttonEnter.setOnClickListener {
+            val intent = Intent(this, CreateAccount::class.java)
+            startActivity(intent)
+        }
     }
 }
