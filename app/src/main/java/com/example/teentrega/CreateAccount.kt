2d284185ae0d3a5a7b255887e882c816d7163077
Gabriela Matias <jasbrela@gmail.com>
2023-04-24 01:00:30 -0300

    adjust dark theme, use spannable strings in transparent buttons

--- app/src/main/java/com/example/teentrega/CreateAccount.kt
+++ app/src/main/java/com/example/teentrega/CreateAccount.kt
@@ -1,7 +1,10 @@
 package com.example.teentrega
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.appcompat.app.AppCompatActivity
 import com.example.teentrega.databinding.ActivityCreateAccountBinding
 
 class CreateAccount : AppCompatActivity() {
@@ -9,6 +12,13 @@ class CreateAccount : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityCreateAccountBinding.inflate(layoutInflater)
+
+        val spannable = SpannableString("${getString(R.string.dont_have_account)} ${getString(R.string.create)}")
+        spannable.setSpan(
+            ForegroundColorSpan(getColor(R.color.text_main)),
+            0,20, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        binding.buttonLogin.text = spannable
+
         setContentView(binding.root)
     }
 
--- app/src/main/java/com/example/teentrega/MainActivity.kt
+++ app/src/main/java/com/example/teentrega/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.teentrega
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.example.teentrega.databinding.ActivityMainBinding
 
@@ -10,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val spannable = SpannableString("${getString(R.string.already_have_account)} ${getString(R.string.enter)}")
+        spannable.setSpan(ForegroundColorSpan(getColor(R.color.text_main)),
+            0,19, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        binding.buttonLogin.text = spannable
+
         setContentView(binding.root)
 
         binding.buttonLogin.setOnClickListener {
