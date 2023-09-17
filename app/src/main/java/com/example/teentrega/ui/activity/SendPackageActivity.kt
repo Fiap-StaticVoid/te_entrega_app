cfc0191b5d60942df8ae3de89c733cedd1bb4872
Gabriela Matias <jasbrela@gmail.com>
2023-09-17 12:56:10 -0300

    start send package part

--- app/src/main/java/com/example/teentrega/ui/activity/SendPackageActivity.kt
+++ app/src/main/java/com/example/teentrega/ui/activity/SendPackageActivity.kt
@@ -2,15 +2,29 @@ package com.example.teentrega.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.teentrega.R
+import com.example.teentrega.api.CallBackOrigin
+import com.example.teentrega.api.CallBackPerOrigin
+import com.example.teentrega.api.ClientAPI
+import com.example.teentrega.api.Method
+import com.example.teentrega.api.Shipping
+import com.example.teentrega.api.ShippingAPI
 import com.example.teentrega.databinding.ActivitySendPackageBinding
+import com.example.teentrega.utils.Constants
+import com.example.teentrega.viewmodel.AccountViewModel
+import com.example.teentrega.viewmodel.PackageViewModel
+import org.json.JSONObject
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class SendPackageActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private val viewModel: PackageViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +34,30 @@ class SendPackageActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(binding.navHost.id) as NavHostFragment
         navController = navHostFragment.navController
 
+        binding.buttonContinue.text = getString(R.string.finish)
+
+        fun update(info: JSONObject) {
+            val intent = Intent(this, FinishSendPackageActivity::class.java)
+            this@SendPackageActivity.finish()
+            startActivity(intent)
+        }
+
+        fun checkInput() {
+            binding.buttonContinue.isEnabled = viewModel.cep.value.toString().length == 8 && viewModel.address.value != "" && viewModel.number.value != ""
+        }
+
+        viewModel.cep.observe(this) { _ -> checkInput() }
+        viewModel.address.observe(this) { _ -> checkInput() }
+        viewModel.number.observe(this) { _ -> checkInput() }
+
+        val mutable : CallBackPerOrigin = mutableMapOf()
+        mutable[CallBackOrigin("entregas/", Method.POST)] = mutableListOf(::update)
+
+        val shippingAPI = ShippingAPI(Constants.IP, mutable)
+
         binding.buttonContinue.setOnClickListener { it ->
-            if (navController.currentDestination?.id != R.id.payment) {
-                val nextFragment = when (navController.currentDestination?.id) {
-                    R.id.address -> R.id.payment
-                    else -> R.id.payment
-                }
-
-                nextFragment.let {
-                    navController.navigate(it)
-                }
-
-                if (it.id == R.id.create_account_fragment_5) {
-                    binding.buttonContinue.text = getString(R.string.finish)
-                }
-            } else {
-                val intent = Intent(this, FinishSendPackageActivity::class.java)
-                this@SendPackageActivity.finish()
-                startActivity(intent)
-            }
+            shippingAPI.create(Shipping(null, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                LocalDateTime.now().plusDays(7).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")), emptyList(), "", ""))
         }
 
         setContentView(binding.root)
--- app/src/main/java/com/example/teentrega/viewmodel/AccountViewModel.kt
+++ app/src/main/java/com/example/teentrega/viewmodel/AccountViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.teentrega.ui
+package com.example.teentrega.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
