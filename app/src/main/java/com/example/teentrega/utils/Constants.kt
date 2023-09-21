f266fea753a8fcd558ee3f1c35f4038b12195b05
Gabriela Matias <jasbrela@gmail.com>
2023-09-21 20:14:31 -0300

    Update Constants.kt

--- app/src/main/java/com/example/teentrega/utils/Constants.kt
+++ app/src/main/java/com/example/teentrega/utils/Constants.kt
@@ -1,8 +1,8 @@
-package com.example.teentrega.utils
+    package com.example.teentrega.utils
 
 class Constants {
     companion object {
-        const val IP = "http://172.30.16.1:8000"
+        const val IP = "http://localhost:8000"
         const val SHARED_PREF = "SharedPreferences"
         const val BEARER_TOKEN = "BearerToken"
     }
